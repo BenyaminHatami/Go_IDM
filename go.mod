ddc5450f13f19268eac7d180cc6361294bf453c1
Parsa <parsagholami138396@gmail.com>
2025-02-27 19:34:09 +0330

    basic programm

--- go.mod
+++ go.mod
@@ -1,15 +1,19 @@
 module myproject
 
-go 1.20
+go 1.22
+
+toolchain go1.23.3
 
 require (
 	github.com/gdamore/encoding v1.0.0 // indirect
 	github.com/gdamore/tcell/v2 v2.7.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/rivo/tview v0.0.0-20241227133733-17b7edb88c57 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
-	golang.org/x/sys v0.17.0 // indirect
-	golang.org/x/term v0.17.0 // indirect
+	github.com/schollz/progressbar/v3 v3.18.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/term v0.29.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,95 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+	"time"
+
+	"github.com/schollz/progressbar/v3"
+)
+
+func downloadFile(url string) error {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("error making request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %s", resp.Status)
+	}
+
+	totalSize := resp.ContentLength
+	if totalSize <= 0 {
+		return fmt.Errorf("unknown file size")
+	}
+
+	filename := path.Base(url)
+	if filename == "" || filename == "." || filename == "/" {
+		filename = "downloaded_file"
+	}
+
+	out, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("error creating file: %v", err)
+	}
+	defer out.Close()
+
+	bar := progressbar.NewOptions64(
+		totalSize,
+		progressbar.OptionSetWidth(30),
+		progressbar.OptionShowBytes(true),
+		progressbar.OptionShowCount(),
+		progressbar.OptionSetDescription(fmt.Sprintf("Downloading %s", filename)),
+		progressbar.OptionEnableColorCodes(true),
+		progressbar.OptionShowElapsedTimeOnFinish(),
+		progressbar.OptionSetTheme(progressbar.Theme{
+			Saucer:        "=",
+			SaucerHead:    ">",
+			SaucerPadding: " ",
+			BarStart:      "[",
+			BarEnd:        "]",
+		}),
+	)
+
+	start := time.Now()
+	multiWriter := io.MultiWriter(out, bar)
+
+	reader := io.TeeReader(resp.Body, multiWriter)
+
+	_, err = io.Copy(out, reader)
+	if err != nil {
+		return fmt.Errorf("error downloading file: %v", err)
+	}
+
+	bar.Finish()
+	fmt.Println()
+
+	elapsed := time.Since(start)
+	speed := float64(totalSize) / elapsed.Seconds() / 1024 // KB/s
+	fmt.Printf("Download completed in %v\n", elapsed.Round(time.Second))
+	fmt.Printf("Average speed: %.2f KB/s\n", speed)
+
+	return nil
+}
 
 func main() {
-	fmt.Println("kos ammaton!")
+	var url string
+	fmt.Println("Enter the URL of the file to download:")
+	fmt.Scanln(&url)
+
+	if url == "" {
+		fmt.Println("Please provide a valid URL")
+		return
+	}
+
+	err := downloadFile(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
